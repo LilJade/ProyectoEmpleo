13df09a82945685f6e24fc07ba8340d583f2e4c6
marvin <marvin@192.168.56.1>
2019-09-17 12:10:46 -0600

    Craecion de entidad Empleo

--- DiseñoDeProyecto/src/Entidades/Empleo.java
+++ DiseñoDeProyecto/src/Entidades/Empleo.java
@@ -10,5 +10,56 @@ package Entidades;
  * @author josue
  */
 public class Empleo {
-    
+   int idEmpleo, idEmpresa;
+   String nombre,descripcion,requisitos;
+   double salario;
+
+    public int getIdEmpleo() {
+        return idEmpleo;
+    }
+
+    public void setIdEmpleo(int idEmpleo) {
+        this.idEmpleo = idEmpleo;
+    }
+
+    public int getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(int idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRequisitos() {
+        return requisitos;
+    }
+
+    public void setRequisitos(String requisitos) {
+        this.requisitos = requisitos;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+   
 }
