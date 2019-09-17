278e8574d95ab962a2da85ded2553916efd21504
marvin <marvin@192.168.56.1>
2019-09-17 12:27:57 -0600

    Craecion de entidad Estudio y Experiencia

--- DiseñoDeProyecto/src/Entidades/Estudio.java
+++ DiseñoDeProyecto/src/Entidades/Estudio.java
@@ -10,5 +10,55 @@ package Entidades;
  * @author josue
  */
 public class Estudio {
+    int idEstudio,idTrabajador;
+    String institucion,titulo,añoTerminado,cuidad;
+
+    public int getIdEstudio() {
+        return idEstudio;
+    }
+
+    public void setIdEstudio(int idEstudio) {
+        this.idEstudio = idEstudio;
+    }
+
+    public int getIdTrabajador() {
+        return idTrabajador;
+    }
+
+    public void setIdTrabajador(int idTrabajador) {
+        this.idTrabajador = idTrabajador;
+    }
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAñoTerminado() {
+        return añoTerminado;
+    }
+
+    public void setAñoTerminado(String añoTerminado) {
+        this.añoTerminado = añoTerminado;
+    }
+
+    public String getCuidad() {
+        return cuidad;
+    }
+
+    public void setCuidad(String cuidad) {
+        this.cuidad = cuidad;
+    }
     
 }
--- DiseñoDeProyecto/src/Entidades/Experiencia.java
+++ DiseñoDeProyecto/src/Entidades/Experiencia.java
@@ -5,10 +5,71 @@
  */
 package Entidades;
 
+import java.util.Date;
+
 /**marvin hace el set y el get 
  *
  * @author josue
  */
 public class Experiencia {
-    
+    int idExperiencia,idTrabajador;
+    String nombreEmpresa,cargoOcupado,descripcion;
+    Date fechaInicio,fechaFinal;
+
+    public int getIdExperiencia() {
+        return idExperiencia;
+    }
+
+    public void setIdExperiencia(int idExperiencia) {
+        this.idExperiencia = idExperiencia;
+    }
+
+    public int getIdTrabajador() {
+        return idTrabajador;
+    }
+
+    public void setIdTrabajador(int idTrabajador) {
+        this.idTrabajador = idTrabajador;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getCargoOcupado() {
+        return cargoOcupado;
+    }
+
+    public void setCargoOcupado(String cargoOcupado) {
+        this.cargoOcupado = cargoOcupado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(Date fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+            
 }
